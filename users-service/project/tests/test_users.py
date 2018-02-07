30a35ccdb9ae37a3fe6c54f24fa4e1879caf51b6
Shern <shihern@hotmail.com>
2018-02-07 18:58:09 +0800

    Added index.html

--- users-service/project/tests/test_users.py
+++ users-service/project/tests/test_users.py
@@ -135,5 +135,30 @@ class TestUserService(BaseTestCase):
             'fletcher@realpython.com', data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
+    def test_main_no_users(self):
+        """Ensure the main route behaves correctly when no users have been added to the database."""
+        add_user('michael', 'michael@realpython.com')
+        add_user('fletcher', 'fletcher@realpython.com')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'michael', response.data)
+            self.assertIn(b'fletcher', response.data)
+
+    def test_main_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+            '/',
+            data=dict(username='michael', email='michael@realpython.com'),
+            follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'michael', response.data)
+
 if __name__ == '__main__':
     unittest.main()
